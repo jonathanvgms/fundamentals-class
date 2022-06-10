8badd5769b79005a157d3266a884e38c34c078d0
jvelazquez <jonathanvgms@gmail.com>
2022-06-09 22:07:09 -0300

    Add class 2

---
+++ Clase2/Motocicleta.cs
@@ -0,0 +1,72 @@
+﻿namespace Clase2
+{
+    public class Motocicleta
+    {
+        //atributos - caracteristicas
+
+        public string patente { get; set; } //forma condesada de escribir metodos getter y setter
+
+        public string color { get; set; }
+        //public string color //forma explayada de escribir metodos getter y setter
+        //{
+        //    get { return color; }
+        //    set {
+        //        color = value;
+        //        //if (value != "")
+        //        //    color = value;
+        //        //else
+        //        //    Console.WriteLine("Color invalido");
+        //    }
+        //}
+
+        private string modelo;
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        private bool estadoMotor = false; // true = motor encendido y false = motor apagado
+
+        //comportamiento
+        public Motocicleta(string unaPatente, string unColor, string unModelo)
+        {
+            patente = unaPatente;
+            this.color = unColor;
+            this.modelo = unModelo;
+            //this.estadoMotor = false; 
+            // esta asignacion depende de que se utilice este constructor al momento
+            //de la instanciacion del objeto
+        }
+
+        public Motocicleta(string unaPatente)
+        {
+            this.patente = unaPatente;
+        }
+
+        public void arrancar()
+        {
+            //if (estadoMotor == false)
+            if (!estadoMotor)
+                estadoMotor = true;
+            else
+                throw new Exception("El motor se encuentra encendido");
+        }
+
+        public void apagar()
+        {
+            if (estadoMotor)
+                estadoMotor = false;
+            else
+                throw new Exception("El motor se encuentra apagado");
+        }
+
+        public bool verEstadoMotor() => estadoMotor;
+
+        public string informar()
+        {
+            return $"Motocicleta de modelo: {modelo} con patente: {patente}  y de color {color}";
+        }
+    }
+}
---
+++ Clase2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Clase2;
+
+//instanciar un objeto motocicleta (en una linea)
+//Motocicleta motocicleta1 = new Motocicleta("ASD 123", "negro", "1999");
+
+//instanciar un objeto motocicleta (en dos lineas)
+//Motocicleta motocicleta2;
+
+//motocicleta2 = new Motocicleta("QWE 345", "azul", "2022");
+
+//instancia un objeto motocicleta (unicamente con la patente)
+Motocicleta motocicleta3 = new Motocicleta("ZXC 123");
+motocicleta3.Modelo = "2004";
+
+Console.WriteLine(motocicleta3.Modelo);
+motocicleta3.color = "Verde";
+Console.WriteLine(motocicleta3.color);
+Console.WriteLine(motocicleta3.patente);
+
+Console.WriteLine($"Estado: {motocicleta3.verEstadoMotor()}");
+
+motocicleta3.arrancar();
+motocicleta3.arrancar(); //tira excepcion
+
+Console.WriteLine($"Estado: {motocicleta3.verEstadoMotor()}");
+motocicleta3.apagar();
+Console.WriteLine($"Estado: {motocicleta3.verEstadoMotor()}");
+Console.WriteLine(motocicleta3.informar());
+
+Console.ReadKey();
